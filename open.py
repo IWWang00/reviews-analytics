42727925d35dbc3e408e5b277245d02d68fe87ce
I-Wen Wang <iwwg10991@gmail.com>
2022-04-22 16:47:59 +0800

    add filter

--- open.py
+++ open.py
@@ -14,4 +14,9 @@ for d in data:
 	sum_len = sum_len + len(d)
 print('留言平均長度是', sum_len/len(data))
 
-print(comment[0])
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
